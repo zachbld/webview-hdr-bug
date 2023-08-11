5f8f2573ecc23f553fe14aaea8b7714b6fa341a7
Zach Marasigan <zach@gonative.io>
2023-08-11 17:49:18 +0800

    Init

---
+++ app/src/main/java/com/example/hdrwebviewbug/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.hdrwebviewbug;
+
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        WebView webView = findViewById(R.id.webView);
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webView.loadUrl("file:///android_asset/video_player.html");
+    }
+}
